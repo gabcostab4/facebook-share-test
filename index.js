f1617ac7cc6b9393d391b273095539061fcd8cbd
gabcostasilva <gabcostadeveloper@gmail.com>
2020-06-01 10:05:46 -0400

    add render props

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    url: "https://bepdv-api.herokuapp.com/",
+    type: 'article',
+    title: 'Claro Promo',
+    description: 'Faça sua renda mensal agora mesmo.',
+    image: 'https://bepdv-api.herokuapp.com/images/banner.jpg',
+    image_width: '1080px',
+    image_height: '420px'
+  })
 })
